524400a808f7d78b73129a78139b3304125c61cc
Awood549 <Austin.wood1776@live.com>
2019-05-18 14:59:38 -0700

    Styling

--- data/books.sql
+++ data/books.sql
@@ -11,4 +11,6 @@ CREATE TABLE books (
 );
 
 INSERT INTO books (title, author, isbn, image, description, bookshelf)
-VALUES('bookOne', 'authorOne', '1234', 'imageLink', 'description???', 'shelfthingy');
+VALUES('boring Book', 'authorOne', '1234', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7OW4AA5HsjrilYT86d5WFYcooJYccLY2ot-eVV71s81KZrWYD', 'The Exciting world of math', 'shelfthingy');
+INSERT INTO books (title, author, isbn, image, description, bookshelf)
+VALUES('bookTwo', 'authorTwo', '5678', 'https://cdn6.picryl.com/photo/1895/12/31/second-jungle-book-book-cover-f9b55f-1600.jpg', 'This is the best book ever exepct for any book ever published', 'shelfthingy');
